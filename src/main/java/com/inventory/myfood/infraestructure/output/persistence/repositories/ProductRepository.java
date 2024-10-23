92b2fce438e802c7df7403f9814a87dab3cd7262
Santiago <sbenitez1607@gmail.com>
2024-10-22 21:39:28 -0500

    Add: Interfaz Repository de Spring Data JPA, que permite la conexión con la base de datos SQL

---
+++ src/main/java/com/inventory/myfood/infraestructure/output/persistence/repositories/ProductRepository.java
@@ -0,0 +1,25 @@
+package com.inventory.myfood.infraestructure.output.persistence.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.inventory.myfood.infraestructure.output.persistence.entities.ProductEntity;
+
+import java.util.List;
+import java.util.Date;
+
+/**
+ * @brief Interfaz ligada a la tecnología (Spring JPA) para gestionar la
+ *        comunicación entre el proyecto y la base de datos.
+ * @details Su función es comunicar la entidad "Product" con la tecnología de
+ *          persistencia en este caso MySQL.
+ */
+public interface ProductRepository extends JpaRepository<ProductEntity, String> {
+    List<ProductEntity> findAllByProductExpiredIsFalse();
+
+    List<ProductEntity> findAllByProductExpiredIsFalseAndProductStockGreaterThan(int productStock);
+
+    List<ProductEntity> findAllByProductExpiredIsTrue();
+
+    List<ProductEntity> findAllByProductUseFulLifeBefore(Date date);
+
+}
