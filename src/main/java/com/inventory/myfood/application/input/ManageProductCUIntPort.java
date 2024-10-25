811142ed1292fba35a83bacaceb6431d0051e37b
Santiago <sbenitez1607@gmail.com>
2024-10-24 19:26:24 -0500

    Fix: Se agregan métodos faltantes, incrementar, decrementar, verificar categoria.

--- src/main/java/com/inventory/myfood/application/input/ManageProductCUIntPort.java
+++ src/main/java/com/inventory/myfood/application/input/ManageProductCUIntPort.java
@@ -32,6 +32,42 @@ public interface ManageProductCUIntPort {
     Product changeName(String uuid, String name);
 
     /**
+     * Se encarga de la lógica de salida de los productos del inventario.
+     * 
+     * @param amount cantidad de unidades salidas del inventario
+     * @return {@code true} si se completa satisfactoriamente y {@code false} en
+     *         caso contrario.
+     */
+    boolean decreaseStock(Product product, int amount);
+
+    /**
+     * Se encarga de la lógica de agregar productos al inventario.
+     * 
+     * @param amount cantidad de unidades a agregar al inventario.
+     * @return {@code true} en caso de que se complete la acción y {@code false} en
+     *         caso contrario.
+     */
+    boolean increaseStock(Product product, int amount);
+
+    /**
+     * Se encarga de determinar si un producto ha llegado a su fecha de caducidad y
+     * lo marca de esta forma
+     * 
+     * @return {@code true} en caso de que el producto se haya vencido o
+     *         {@code false} en caso contrario.
+     */
+    public boolean markExpired(Product product);
+
+    /**
+     * Determina si la categoria ingresada es válida
+     * 
+     * @param category categoria del alimento
+     * @return {@code true} en caso de que la categoria este registrada y
+     *         {@code false} en caso contrario.
+     */
+    boolean isCategory(String category);
+
+    /**
      * Recupera todos los productos que se han registrado en el sistema.
      * 
      * @return {@code List<Product>} con la información del inventario o
