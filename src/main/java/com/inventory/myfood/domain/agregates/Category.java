c687129d45d3867d1f1801da566eb3377c26228f
Santiago <sbenitez1607@gmail.com>
2024-11-04 16:34:27 -0500

    Remove: atributo allow decimal de Category, eso es algo que pertenece a las unidades.

--- src/main/java/com/inventory/myfood/domain/agregates/Category.java
+++ src/main/java/com/inventory/myfood/domain/agregates/Category.java
@@ -8,25 +8,18 @@ import lombok.Getter;
 public class Category {
     private String id;
     private CategoryName name;
-    private boolean allowDecimals = true;
 
-    public Category(String id, String name, boolean allowDecimals) {
+    public Category(String id, String name) {
         this.id = id;
         this.name = new CategoryName(name);
-        this.allowDecimals = allowDecimals;
     }
 
     public void changeName(String name) {
         this.name = new CategoryName(name);
     }
 
-    public void changeAllowDecimals() {
-        this.allowDecimals = !allowDecimals;
-    }
-
     public void update(String name, boolean allowDecimals) {
         this.name = new CategoryName(name);
-        this.allowDecimals = allowDecimals;
     }
 
     public boolean isValidname(String name) {
